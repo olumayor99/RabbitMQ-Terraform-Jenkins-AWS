95263989760ee54b9713f6797b6e1dbdfd4769e5
Olumayowa Taiwo <olumayoor99@gmail.com>
2022-05-16 10:12:50 +0300

    corrections

--- terraform/rmq/variables.tf
+++ terraform/rmq/variables.tf
@@ -1,11 +1,14 @@
 variable "name" {
     description = "name the instance on deploy"
+    default = "rmq"
 }
 
 variable "group" {
     description = "The group tag for ansible to identify RMQ"
+    default = "rmq"
 }
 
 variable "profile" {
     description = "Profile to use for deployment"
+    default = "DevOps"
 }
